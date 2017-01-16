534656d48f356a09e4ba2a771b89afe8e123bd23
1cookj <jcook1@us.ibm.com>
2017-01-16 17:41:10 -0500

    add audio files and copy of device file

--- app/public/javascripts/iotAppMonitorClient.js
+++ app/public/javascripts/iotAppMonitorClient.js
@@ -1,29 +1,27 @@
 //
-// client library for receiving monitor messages. 
+// client library for receiving monitor messages.
 //
 var iotAppMonitorClient = (function () {
-	
+
 	var result = {};
-	
+
 	var socket = io.connect(window.location.host, {transports:['websocket']});
-	
+
 	if (socket){
-		
+
 		socket.on('iotwb-http', function (data) {
 			if (typeof result.http === 'function'){
 				result.http(data.message);
 			}
   		});
-  		
+
   		socket.on('iotwb-mqtt', function (data) {
+			console.log('iotMonitorclient=' + JSON.stringify(data));
 			if (typeof result.mqtt === 'function'){
 				result.mqtt(data.id, data.message);
 			}
   		});
     }
-	
+
 	return result;
 })();
-
-
-  
